6555516a339fd42ef165e577d35dfc2e6c081c19
mark mwendwa <mwendwamark310@gmail.com>
2023-06-26 17:28:35 +0300

    done

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,18 @@
 class ClientsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def show
+    client = finder
+    render json: client, status: :ok, except: [:created_at, :updated_at], include: { memberships: { except: [:created_at, :updated_at] } }
+  end
+
+  private
+
+  def finder
+    client = Client.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Client Not Found" }, status: :not_found
+  end
 end
--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,17 +1,24 @@
 class GymsController < ApplicationController
-rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-    def show 
-        gym = finder 
-        render json: gym 
-    end
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-    private 
+  def show
+    gym = finder
+    render json: gym, except: [:created_at, :updated_at], include: { memberships: { except: [:created_at, :updated_at] } }, status: :ok
+  end
 
-    def render_not_found_response 
-        render json: { error: "Gym not Found"}
-    end
+  def delete
+    delete_gym = finder
+    delete_gym.destroy
+    head :no_content
+  end
 
-    def finder 
-        gym = Gym.find(params[:id])
-    end
+  private
+
+  def render_not_found_response
+    render json: { error: "Gym Not Found Please Try Another Gym" }
+  end
+
+  def finder
+    gym = Gym.find(params[:id])
+  end
 end
